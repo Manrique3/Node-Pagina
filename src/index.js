0c9f846173b7eec262cf82a691bfeb437431397e
Gabriel <alberto.manrique99rgmail.com>
2022-03-31 23:35:53 -0400

    Html, Bootstrap y routas en node.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('Server works');
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+
+
+//Configuracion del puerto del servidor
+app.set('port',4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html',require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+
+
+//Routes
+app.use(require('./routers/index'));
+
+
+//Static files
+
+
+//Escuchando el servidor, iniciando el servidor 
+app.listen(app.set('port'), () =>{
+    console.log('Server on port', app.set('port'));
+});
+
+
---
+++ src/routers/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const router = express.Router();
+
+
+router.get('/', (req, res)=>{
+    res.render('index.html', {title: 'My Website Node.js'});
+});
+
+router.get('/contact', (req, res)=>{
+    res.render('contact.html', {title: 'My contact'});
+});
+
+
+module.exports = router;
